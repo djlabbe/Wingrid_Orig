6a8536dd0e53fa2fc402b1a6e7b7258537a868be
Douglas Labbe <dlabbe@Douglass-MacBook-Pro.local>
2019-06-28 09:16:06 -0700

    start front end design

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>WinGrid</h1>
+          <p className='lead'>Outwit, outplay, outpick... win big!</p>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
+              Sign Up
+            </Link>
+            <Link to='/login' className='btn btn-light'>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
