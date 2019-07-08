8228a7397e2d38b6c21e36bc9151b2a51e485a49
Doug Labbe <dougjlabbe@gmail.com>
2019-07-08 00:09:51 -0700

    setup prod env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,8 +10,6 @@ connectDB();
 // Init middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
@@ -19,6 +18,16 @@ app.use('/api/teams', require('./routes/api/teams'));
 app.use('/api/games', require('./routes/api/games'));
 app.use('/api/sheets', require('./routes/api/sheets'));
 app.use('/api/entry', require('./routes/api/entry'));
+
+// Serve static assets in prod
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
