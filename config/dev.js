49edb1aa97cc46f2815e4c2414a0d89b10c2dd00
Doug Labbe <dougjlabbe@gmail.com>
2019-07-08 23:05:35 -0700

    hide dev keys

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mongoURI:
-    'mongodb+srv://admin:Uu3UmYNDnookzFYi@cluster0-1pjhq.mongodb.net/test?retryWrites=true&w=majority',
-  jwtSecret: 'H2n9fgbAnvKfbxmJ83mSjkG9fn1nf1o3',
-  hooks:
-    'HzIixPRc-YNqw04Sw2QLangBBPVvHrx6REsX7AT5Q4yPigcctHM09w8zWC4am1vNMzA5Mw'
-};
