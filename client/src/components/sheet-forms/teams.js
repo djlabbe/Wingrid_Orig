0af16efb5339d8aac25f9f53761691a8d14887fd
Douglas Labbe <dougjlabbe@gmail.com>
2019-09-10 18:49:12 -0700

    fix ucla typo

--- client/src/components/sheet-forms/teams.js
+++ client/src/components/sheet-forms/teams.js
@@ -1,650 +1,650 @@
 export const teams = [
   {
-    value: 'Arizona Cardinals',
-    label: 'Arizona Cardinals'
+    value: "Arizona Cardinals",
+    label: "Arizona Cardinals"
   },
   {
-    value: 'Atlanta Falcons',
-    label: 'Atlanta Falcons'
+    value: "Atlanta Falcons",
+    label: "Atlanta Falcons"
   },
   {
-    value: 'Baltimore Ravens',
-    label: 'Baltimore Ravens'
+    value: "Baltimore Ravens",
+    label: "Baltimore Ravens"
   },
   {
-    value: 'Buffalo Bills',
-    label: 'Buffalo Bills'
+    value: "Buffalo Bills",
+    label: "Buffalo Bills"
   },
   {
-    value: 'Carolina Panthers',
-    label: 'Carolina Panthers'
+    value: "Carolina Panthers",
+    label: "Carolina Panthers"
   },
   {
-    value: 'Chicago Bears',
-    label: 'Chicago Bears'
+    value: "Chicago Bears",
+    label: "Chicago Bears"
   },
   {
-    value: 'Cincinnati Bengals',
-    label: 'Cincinnati Bengals'
+    value: "Cincinnati Bengals",
+    label: "Cincinnati Bengals"
   },
   {
-    value: 'Cleveland Browns',
-    label: 'Cleveland Browns'
+    value: "Cleveland Browns",
+    label: "Cleveland Browns"
   },
   {
-    value: 'Dallas Cowboys',
-    label: 'Dallas Cowboys'
+    value: "Dallas Cowboys",
+    label: "Dallas Cowboys"
   },
   {
-    value: 'Denver Broncos',
-    label: 'Denver Broncos'
+    value: "Denver Broncos",
+    label: "Denver Broncos"
   },
   {
-    value: 'Detroit Lions',
-    label: 'Detroit Lions'
+    value: "Detroit Lions",
+    label: "Detroit Lions"
   },
   {
-    value: 'Green Bay Packers',
-    label: 'Green Bay Packers'
+    value: "Green Bay Packers",
+    label: "Green Bay Packers"
   },
   {
-    value: 'Houston Texans',
-    label: 'Houston Texans'
+    value: "Houston Texans",
+    label: "Houston Texans"
   },
   {
-    value: 'Indianapolis Colts',
-    label: 'Indianapolis Colts'
+    value: "Indianapolis Colts",
+    label: "Indianapolis Colts"
   },
   {
-    value: 'Jacksonville Jaguars',
-    label: 'Jacksonville Jagurars'
+    value: "Jacksonville Jaguars",
+    label: "Jacksonville Jagurars"
   },
   {
-    value: 'Kansas City Chiefs',
-    label: 'Kansas City Chiefs'
+    value: "Kansas City Chiefs",
+    label: "Kansas City Chiefs"
   },
   {
-    value: 'Miami Dolphins',
-    label: 'Miami Dolphins'
+    value: "Miami Dolphins",
+    label: "Miami Dolphins"
   },
   {
-    value: 'Minnesota Vikings',
-    label: 'Minnesota Vikings'
+    value: "Minnesota Vikings",
+    label: "Minnesota Vikings"
   },
   {
-    value: 'New England Patriots',
-    label: 'New England Patriots'
+    value: "New England Patriots",
+    label: "New England Patriots"
   },
   {
-    value: 'New Orleans Saints',
-    label: 'New Orleans Saints'
+    value: "New Orleans Saints",
+    label: "New Orleans Saints"
   },
   {
-    value: 'New York Giants',
-    label: 'New York Giants'
+    value: "New York Giants",
+    label: "New York Giants"
   },
   {
-    value: 'New York Jets',
-    label: 'New York Jets'
+    value: "New York Jets",
+    label: "New York Jets"
   },
   {
-    value: 'Oakland Raiders',
-    label: 'Oakland Raiders'
+    value: "Oakland Raiders",
+    label: "Oakland Raiders"
   },
   {
-    value: 'Philadelphia Eagles',
-    label: 'Philadelphia Eagles'
+    value: "Philadelphia Eagles",
+    label: "Philadelphia Eagles"
   },
   {
-    value: 'Pittsburgh Steelers',
-    label: 'Pittsburgh Steelers'
+    value: "Pittsburgh Steelers",
+    label: "Pittsburgh Steelers"
   },
   {
-    value: 'Los Angeles Chargers',
-    label: 'Los Angeles Chargers'
+    value: "Los Angeles Chargers",
+    label: "Los Angeles Chargers"
   },
   {
-    value: 'San Francisco 49ers',
-    label: 'San Francisco 49ers'
+    value: "San Francisco 49ers",
+    label: "San Francisco 49ers"
   },
   {
-    value: 'Seattle Seahawks',
-    label: 'Seattle Seahawks'
+    value: "Seattle Seahawks",
+    label: "Seattle Seahawks"
   },
   {
-    value: 'Los Angeles Rams',
-    label: 'Los Angeles Rams'
+    value: "Los Angeles Rams",
+    label: "Los Angeles Rams"
   },
   {
-    value: 'Tampa Bay Buccanneers',
-    label: 'Tampa Bay Buccaneers'
+    value: "Tampa Bay Buccanneers",
+    label: "Tampa Bay Buccaneers"
   },
   {
-    value: 'Tennessee Titans',
-    label: 'Tennessee Titans'
+    value: "Tennessee Titans",
+    label: "Tennessee Titans"
   },
   {
-    value: 'Washington Redskins',
-    label: 'Washington Redskins'
+    value: "Washington Redskins",
+    label: "Washington Redskins"
   },
   {
-    value: 'Air Force',
-    label: 'Air Force'
+    value: "Air Force",
+    label: "Air Force"
   },
   {
-    value: 'Akron',
-    label: 'Akron'
+    value: "Akron",
+    label: "Akron"
   },
   {
-    value: 'Alabama',
-    label: 'Alabama'
+    value: "Alabama",
+    label: "Alabama"
   },
   {
-    value: 'Alabama Birmingham-(UAB)',
-    label: 'Alabama Birmingham-(UAB)'
+    value: "Alabama Birmingham-(UAB)",
+    label: "Alabama Birmingham-(UAB)"
   },
   {
-    value: 'Appalachian State',
-    label: 'Appalachian State'
+    value: "Appalachian State",
+    label: "Appalachian State"
   },
   {
-    value: 'Arizona',
-    label: 'Arizona'
+    value: "Arizona",
+    label: "Arizona"
   },
   {
-    value: 'Arizona State',
-    label: 'Arizona State'
+    value: "Arizona State",
+    label: "Arizona State"
   },
   {
-    value: 'Arkansas',
-    label: 'Arkansas'
+    value: "Arkansas",
+    label: "Arkansas"
   },
   {
-    value: 'Arkansas State',
-    label: 'Arkansas State'
+    value: "Arkansas State",
+    label: "Arkansas State"
   },
   {
-    value: 'Army',
-    label: 'Army'
+    value: "Army",
+    label: "Army"
   },
   {
-    value: 'Auburn',
-    label: 'Auburn'
+    value: "Auburn",
+    label: "Auburn"
   },
   {
-    value: 'Ball State',
-    label: 'Ball State'
+    value: "Ball State",
+    label: "Ball State"
   },
   {
-    value: 'Baylor',
-    label: 'Baylor'
+    value: "Baylor",
+    label: "Baylor"
   },
   {
-    value: 'Boise State',
-    label: 'Boise State'
+    value: "Boise State",
+    label: "Boise State"
   },
   {
-    value: 'Boston College',
-    label: 'Boston College'
+    value: "Boston College",
+    label: "Boston College"
   },
   {
-    value: 'Bowling Green',
-    label: 'Bowling Green'
+    value: "Bowling Green",
+    label: "Bowling Green"
   },
   {
-    value: 'Buffalo',
-    label: 'Buffalo'
+    value: "Buffalo",
+    label: "Buffalo"
   },
   {
-    value: 'Brigham Young',
-    label: 'Brigham Young'
+    value: "Brigham Young",
+    label: "Brigham Young"
   },
   {
-    value: 'California',
-    label: 'California'
+    value: "California",
+    label: "California"
   },
   {
-    value: 'Californa, Los Angeles',
-    label: 'Californa, Los Angeles'
+    value: "California, Los Angeles",
+    label: "California, Los Angeles"
   },
   {
-    value: 'Central Florida',
-    label: 'Central Florida'
+    value: "Central Florida",
+    label: "Central Florida"
   },
   {
-    value: 'Central Michigan',
-    label: 'Central Michigan'
+    value: "Central Michigan",
+    label: "Central Michigan"
   },
   {
-    value: 'Charlotte',
-    label: 'Charlotte'
+    value: "Charlotte",
+    label: "Charlotte"
   },
   {
-    value: 'Cincinnati',
-    label: 'Cincinnati'
+    value: "Cincinnati",
+    label: "Cincinnati"
   },
   {
-    value: 'Clemson',
-    label: 'Clemson'
+    value: "Clemson",
+    label: "Clemson"
   },
   {
-    value: 'Coastal Carolina',
-    label: 'Coastal Carolina'
+    value: "Coastal Carolina",
+    label: "Coastal Carolina"
   },
   {
-    value: 'Colorado',
-    label: 'Colorado'
+    value: "Colorado",
+    label: "Colorado"
   },
   {
-    value: 'Colorado State',
-    label: 'Colorado State'
+    value: "Colorado State",
+    label: "Colorado State"
   },
   {
-    value: 'Connecticut',
-    label: 'Connecticut'
+    value: "Connecticut",
+    label: "Connecticut"
   },
   {
-    value: 'Duke',
-    label: 'Duke'
+    value: "Duke",
+    label: "Duke"
   },
   {
-    value: 'Eastern Michigan',
-    label: 'Eastern Michigan'
+    value: "Eastern Michigan",
+    label: "Eastern Michigan"
   },
   {
-    value: 'East Carolina',
-    label: 'East Carolina'
+    value: "East Carolina",
+    label: "East Carolina"
   },
   {
-    value: 'Florida International',
-    label: 'Florida International'
+    value: "Florida International",
+    label: "Florida International"
   },
   {
-    value: 'Florida',
-    label: 'Florida'
+    value: "Florida",
+    label: "Florida"
   },
   {
-    value: 'Florida Atlantic',
-    label: 'Florida Atlantic'
+    value: "Florida Atlantic",
+    label: "Florida Atlantic"
   },
   {
-    value: 'Florida State',
-    label: 'Florida State'
+    value: "Florida State",
+    label: "Florida State"
   },
   {
-    value: 'Fresno State',
-    label: 'Fresno State'
+    value: "Fresno State",
+    label: "Fresno State"
   },
   {
-    value: 'Georgia',
-    label: 'Georgia'
+    value: "Georgia",
+    label: "Georgia"
   },
   {
-    value: 'Georgia Southern',
-    label: 'Georgia Southern'
+    value: "Georgia Southern",
+    label: "Georgia Southern"
   },
   {
-    value: 'Georgia State',
-    label: 'Georgia State'
+    value: "Georgia State",
+    label: "Georgia State"
   },
   {
-    value: 'Georgia Tech',
-    label: 'Georgia Tech'
+    value: "Georgia Tech",
+    label: "Georgia Tech"
   },
   {
-    value: 'Hawaii',
-    label: 'Hawaii'
+    value: "Hawaii",
+    label: "Hawaii"
   },
   {
-    value: 'Houston',
-    label: 'Houston'
+    value: "Houston",
+    label: "Houston"
   },
   {
-    value: 'Illinois',
-    label: 'Illinois'
+    value: "Illinois",
+    label: "Illinois"
   },
   {
-    value: 'Indiana',
-    label: 'Indiana'
+    value: "Indiana",
+    label: "Indiana"
   },
   {
-    value: 'Iowa',
-    label: 'Iowa'
+    value: "Iowa",
+    label: "Iowa"
   },
   {
-    value: 'Iowa State',
-    label: 'Iowa State'
+    value: "Iowa State",
+    label: "Iowa State"
   },
   {
-    value: 'Kansas',
-    label: 'Kansas'
+    value: "Kansas",
+    label: "Kansas"
   },
   {
-    value: 'Kansas State',
-    label: 'Kansas State'
+    value: "Kansas State",
+    label: "Kansas State"
   },
   {
-    value: 'Kent State',
-    label: 'Kent State'
+    value: "Kent State",
+    label: "Kent State"
   },
   {
-    value: 'Kentucky',
-    label: 'Kentucky'
+    value: "Kentucky",
+    label: "Kentucky"
   },
   {
-    value: 'Liberty',
-    label: 'Liberty'
+    value: "Liberty",
+    label: "Liberty"
   },
   {
-    value: 'Louisiana State',
-    label: 'Louisiana State'
+    value: "Louisiana State",
+    label: "Louisiana State"
   },
   {
-    value: 'Louisiana Tech',
-    label: 'Louisiana Tech'
+    value: "Louisiana Tech",
+    label: "Louisiana Tech"
   },
   {
-    value: 'Louisiana',
-    label: 'Louisiana'
+    value: "Louisiana",
+    label: "Louisiana"
   },
   {
-    value: 'Louisiana–Monroe',
-    label: 'Louisiana–Monroe'
+    value: "Louisiana–Monroe",
+    label: "Louisiana–Monroe"
   },
   {
-    value: 'Louisville',
-    label: 'Louisville'
+    value: "Louisville",
+    label: "Louisville"
   },
   {
-    value: 'Marshall',
-    label: 'Marshall'
+    value: "Marshall",
+    label: "Marshall"
   },
   {
-    value: 'Maryland',
-    label: 'Maryland'
+    value: "Maryland",
+    label: "Maryland"
   },
   {
-    value: 'Massachusetts',
-    label: 'Massachusetts'
+    value: "Massachusetts",
+    label: "Massachusetts"
   },
   {
-    value: 'Memphis',
-    label: 'Memphis'
+    value: "Memphis",
+    label: "Memphis"
   },
   {
-    value: 'Miami (FL)',
-    label: 'Miami (FL)'
+    value: "Miami (FL)",
+    label: "Miami (FL)"
   },
   {
-    value: 'Miami (OH)',
-    label: 'Miami (OH)'
+    value: "Miami (OH)",
+    label: "Miami (OH)"
   },
   {
-    value: 'Michigan',
-    label: 'Michigan'
+    value: "Michigan",
+    label: "Michigan"
   },
   {
-    value: 'Michigan State',
-    label: 'Michigan State'
+    value: "Michigan State",
+    label: "Michigan State"
   },
   {
-    value: 'Middle Tennessee',
-    label: 'Middle Tennessee'
+    value: "Middle Tennessee",
+    label: "Middle Tennessee"
   },
   {
-    value: 'Minnesota',
-    label: 'Minnesota'
+    value: "Minnesota",
+    label: "Minnesota"
   },
   {
-    value: 'Ole Miss',
-    label: 'Ole Miss'
+    value: "Ole Miss",
+    label: "Ole Miss"
   },
   {
-    value: 'Mississippi State',
-    label: 'Mississippi State'
+    value: "Mississippi State",
+    label: "Mississippi State"
   },
   {
-    value: 'Missouri',
-    label: 'Missouri'
+    value: "Missouri",
+    label: "Missouri"
   },
   {
-    value: 'Navy',
-    label: 'Navy'
+    value: "Navy",
+    label: "Navy"
   },
   {
-    value: 'Nebraska',
-    label: 'Nebraska'
+    value: "Nebraska",
+    label: "Nebraska"
   },
   {
-    value: 'Nevada',
-    label: 'Nevada'
+    value: "Nevada",
+    label: "Nevada"
   },
   {
-    value: 'Nevada Las Vegas',
-    label: 'Nevada Las Vegas'
+    value: "Nevada Las Vegas",
+    label: "Nevada Las Vegas"
   },
   {
-    value: 'New Mexico',
-    label: 'New Mexico'
+    value: "New Mexico",
+    label: "New Mexico"
   },
   {
-    value: 'New Mexico State',
-    label: 'New Mexico State'
+    value: "New Mexico State",
+    label: "New Mexico State"
   },
   {
-    value: 'North Carolina',
-    label: 'North Carolina'
+    value: "North Carolina",
+    label: "North Carolina"
   },
   {
-    value: 'North Carolina State',
-    label: 'North Carolina State'
+    value: "North Carolina State",
+    label: "North Carolina State"
   },
   {
-    value: 'North Texas',
-    label: 'North Texas'
+    value: "North Texas",
+    label: "North Texas"
   },
   {
-    value: 'Northern Illinois',
-    label: 'Northern Illinois'
+    value: "Northern Illinois",
+    label: "Northern Illinois"
   },
   {
-    value: 'Northwestern',
-    label: 'Northwestern'
+    value: "Northwestern",
+    label: "Northwestern"
   },
   {
-    value: 'Notre Dame',
-    label: 'Notre Dame'
+    value: "Notre Dame",
+    label: "Notre Dame"
   },
   {
-    value: 'Ohio',
-    label: 'Ohio'
+    value: "Ohio",
+    label: "Ohio"
   },
   {
-    value: 'Ohio State',
-    label: 'Ohio State'
+    value: "Ohio State",
+    label: "Ohio State"
   },
   {
-    value: 'Oklahoma',
-    label: 'Oklahoma'
+    value: "Oklahoma",
+    label: "Oklahoma"
   },
   {
-    value: 'Oklahoma State',
-    label: 'Oklahoma State'
+    value: "Oklahoma State",
+    label: "Oklahoma State"
   },
   {
-    value: 'Old Dominion',
-    label: 'Old Dominion'
+    value: "Old Dominion",
+    label: "Old Dominion"
   },
   {
-    value: 'Oregon',
-    label: 'Oregon'
+    value: "Oregon",
+    label: "Oregon"
   },
   {
-    value: 'Oregon State',
-    label: 'Oregon State'
+    value: "Oregon State",
+    label: "Oregon State"
   },
   {
-    value: 'Penn State',
-    label: 'Penn State'
+    value: "Penn State",
+    label: "Penn State"
   },
   {
-    value: 'Pittsburgh',
-    label: 'Pittsburgh'
+    value: "Pittsburgh",
+    label: "Pittsburgh"
   },
   {
-    value: 'Purdue',
-    label: 'Purdue'
+    value: "Purdue",
+    label: "Purdue"
   },
   {
-    value: 'Rice',
-    label: 'Rice'
+    value: "Rice",
+    label: "Rice"
   },
   {
-    value: 'Rutgers',
-    label: 'Rutgers'
+    value: "Rutgers",
+    label: "Rutgers"
   },
   {
-    value: 'San Diego State',
-    label: 'San Diego State'
+    value: "San Diego State",
+    label: "San Diego State"
   },
   {
-    value: 'San Jose State',
-    label: 'San Jose State'
+    value: "San Jose State",
+    label: "San Jose State"
   },
   {
-    value: 'South Alabama',
-    label: 'South Alabama'
+    value: "South Alabama",
+    label: "South Alabama"
   },
   {
-    value: 'South Carolina',
-    label: 'South Carolina'
+    value: "South Carolina",
+    label: "South Carolina"
   },
   {
-    value: 'South Florida',
-    label: 'South Florida'
+    value: "South Florida",
+    label: "South Florida"
   },
   {
-    value: 'Southern California',
-    label: 'Southern California'
+    value: "Southern California",
+    label: "Southern California"
   },
   {
-    value: 'Southern Methodist',
-    label: 'Southern Methodist'
+    value: "Southern Methodist",
+    label: "Southern Methodist"
   },
   {
-    value: 'Southern Miss',
-    label: 'Southern Miss'
+    value: "Southern Miss",
+    label: "Southern Miss"
   },
   {
-    value: 'Stanford',
-    label: 'Stanford'
+    value: "Stanford",
+    label: "Stanford"
   },
   {
-    value: 'Syracuse',
-    label: 'Syracuse'
+    value: "Syracuse",
+    label: "Syracuse"
   },
   {
-    value: 'Texas Christian',
-    label: 'Texas Christian'
+    value: "Texas Christian",
+    label: "Texas Christian"
   },
   {
-    value: 'Temple',
-    label: 'Temple'
+    value: "Temple",
+    label: "Temple"
   },
   {
-    value: 'Tennessee',
-    label: 'Tennessee'
+    value: "Tennessee",
+    label: "Tennessee"
   },
   {
-    value: 'Texas',
-    label: 'Texas'
+    value: "Texas",
+    label: "Texas"
   },
   {
-    value: 'Texas A&M',
-    label: 'Texas A&M'
+    value: "Texas A&M",
+    label: "Texas A&M"
   },
   {
-    value: 'Texas State',
-    label: 'Texas State'
+    value: "Texas State",
+    label: "Texas State"
   },
   {
-    value: 'Texas Tech',
-    label: 'Texas Tech'
+    value: "Texas Tech",
+    label: "Texas Tech"
   },
   {
-    value: 'Texas El Paso',
-    label: 'Texas El Paso'
+    value: "Texas El Paso",
+    label: "Texas El Paso"
   },
   {
-    value: 'Texas San Antonio',
-    label: 'Texas San Antonio '
+    value: "Texas San Antonio",
+    label: "Texas San Antonio "
   },
   {
-    value: 'Toledo',
-    label: 'Toledo'
+    value: "Toledo",
+    label: "Toledo"
   },
   {
-    value: 'Troy',
-    label: 'Troy'
+    value: "Troy",
+    label: "Troy"
   },
   {
-    value: 'Tulane',
-    label: 'Tulane'
+    value: "Tulane",
+    label: "Tulane"
   },
   {
-    value: 'Tulsa',
-    label: 'Tulsa'
+    value: "Tulsa",
+    label: "Tulsa"
   },
   {
-    value: 'Utah',
-    label: 'Utah'
+    value: "Utah",
+    label: "Utah"
   },
   {
-    value: 'Utah State',
-    label: 'Utah State'
+    value: "Utah State",
+    label: "Utah State"
   },
   {
-    value: 'Vanderbilt',
-    label: 'Vanderbilt'
+    value: "Vanderbilt",
+    label: "Vanderbilt"
   },
   {
-    value: 'Virginia',
-    label: 'Virginia'
+    value: "Virginia",
+    label: "Virginia"
   },
   {
-    value: 'Virginia Tech',
-    label: 'Virginia Tech'
+    value: "Virginia Tech",
+    label: "Virginia Tech"
   },
   {
-    value: 'Wake Forest',
-    label: 'Wake Forest'
+    value: "Wake Forest",
+    label: "Wake Forest"
   },
   {
-    value: 'Washington',
-    label: 'Washington'
+    value: "Washington",
+    label: "Washington"
   },
   {
-    value: 'Washington State',
-    label: 'Washington State'
+    value: "Washington State",
+    label: "Washington State"
   },
   {
-    value: 'West Virginia',
-    label: 'West Virginia'
+    value: "West Virginia",
+    label: "West Virginia"
   },
   {
-    value: 'Western Kentucky',
-    label: 'Western Kentucky'
+    value: "Western Kentucky",
+    label: "Western Kentucky"
   },
   {
-    value: 'Western Michigan',
-    label: 'Western Michigan'
+    value: "Western Michigan",
+    label: "Western Michigan"
   },
   {
-    value: 'Wisconsin',
-    label: 'Wisconsin'
+    value: "Wisconsin",
+    label: "Wisconsin"
   },
   {
-    value: 'Wyoming',
-    label: 'Wyoming'
+    value: "Wyoming",
+    label: "Wyoming"
   }
 ];
